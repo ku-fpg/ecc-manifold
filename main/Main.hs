c6800216d7f1a3a7ce43d9a986be852cae222ce4
Andy Gill <andygill@ku.edu>
2013-11-29 12:02:06 -0600

    Messing with what we print.

--- main/Main.hs
+++ main/Main.hs
@@ -14,14 +14,8 @@ codes = BPSK.code
 
 main :: IO ()
 main = do
-  eccTester (Options ["bpsk/16"] [0,8] {- ,2,4,6,8,10]-} 0) codes $ \ eccs ebN0s -> do
+  eccTester (Options ["bpsk/16"] [0,8,10] {- ,2,4,6,8,10]-} 0) codes $ \ eccs ebN0s -> do
           -- here is where we would setup any fancy output
           return $ \  ecc ebN0 bes -> do
-                  print (name ecc,ebN0,bitErrorRate ecc bes)
+                  print (name ecc,ebN0,sumBEs bes,sizeBEs bes,bitErrorRate ecc bes)
                   return $ sizeBEs bes > 1000 && sumBEs bes > 1000
-                      -- make sure all points have at least 100 runs behind them
-{-
-        est <- nintyFifth gen (message_length ecc) bers
-        debug 1 $ "Est. BER = " ++ show (estPoint est)
-                     return (ebN0,sizeBEs bers,sumBEs bers,est)
--}
