86efdf7b6ab75641e3d5351ef9046b0a09e34ce1
Andy Gill <andygill@ku.edu>
2013-12-26 21:25:32 -0600

    Adding hard/soft option

--- src/ECC/Code/Repetition.hs
+++ src/ECC/Code/Repetition.hs
@@ -4,23 +4,33 @@ module ECC.Code.Repetition where
 import Data.Bit
 import ECC.Types
 import Data.Char (isDigit)
+import Data.Monoid
 
 -- Simple BSPK encode/decode.
-mkRepetition :: Int -> ECC
-mkRepetition n = ECC
-        { name     = "repetition/" ++ show n
+mkHardRepetition :: Int -> ECC
+mkHardRepetition n = ECC
+        { name     = "repetition/hard/" ++ show n
         , encode   = return . take n . repeat . head
         , decode   = return . (,True) . (: []) . mkBit
                    . (> (n `div` 2)) . length . filter (== 1) . map hard
---                     . (> 0) . sum
+        , message_length  = 1
+        , codeword_length = n
+        }
+mkSoftRepetition :: Int -> ECC
+mkSoftRepetition n = ECC
+        { name     = "repetition/soft/" ++ show n
+        , encode   = return . take n . repeat . head
+        , decode   = return . (,True) . (: []) . mkBit . (> 0) . sum
         , message_length  = 1
         , codeword_length = n
         }
 
+
 code :: Code
-code = Code ["repetition/<n>"]
+code = Code ["repetition/(hard|soft)/<n>"]
      $ \ xs -> case xs of
-                        ["repetition",n] | all isDigit n -> return [mkRepetition (read n)]
+                        ["repetition","hard",n] | all isDigit n -> return [mkHardRepetition (read n)]
+                        ["repetition","soft",n] | all isDigit n -> return [mkSoftRepetition (read n)]
                         _                                -> return []
 
 
